b0c0e383be24b5d3521c5f2d0c42c08ef35c88ac
jssdeveloper <stalsjanis@gmail.com>
2022-12-22 07:46:06 +0200

    added templates and html index

--- js-flask.py
+++ js-flask.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return"<H1>Janis Stals flask page</H1>"
+	return render_template("index.html")
 
 @app.route("/welcome")
 def welcome():
