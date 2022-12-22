309872192a72bbe202a21d7cec9b643d3680c126
jssdeveloper <stalsjanis@gmail.com>
2022-12-22 06:52:25 +0200

    added all routes for week 1

--- js-flask.py
+++ js-flask.py
@@ -2,5 +2,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+	return"<H1>Janis Stals flask page</H1>"
+
+@app.route("/welcome")
 def welcome():
 	return"<H1>Welcome</H1>"
+
+@app.route("/welcome/home")
+def welcomehome():
+	return"<H1>Welcome home</H1>"
+
+@app.route("/welcome/back")
+def welcomeback():
+	return"<H1>Welcome back</H1>"
