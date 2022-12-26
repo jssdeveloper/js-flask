ccc9c4c184fd916b30174b4954b13b6a5dc812ba
jssdeveloper <stalsjanis@gmail.com>
2022-12-26 21:48:06 +0200

    added circlek prices, some design changes

--- fuelscrape.py
+++ fuelscrape.py
@@ -1,11 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
+#import os
+#os.system("clear")
 
-url_circle = "https://www.circlek.lv/privātpersonām/degvielas-cenas"
-url_neste = "https://www.neste.lv/lv/content/degvielas-cenas"
-url_virsi = "https://www.virsi.lv/lv/privatpersonam/degviela/degvielas-un-elektrouzlades-cenas"
-url_viada = "https://www.viada.lv/zemakas-degvielas-cenas/"
-url_kool = "https://koolbusiness.lv/degvielas-cenu-vesture/"
-url_gotika = "https://www.gotikaauto.lv"
+#url_neste = requests.get("https://www.neste.lv/lv/content/degvielas-cenas")
+#url_virsi = requests.get("https://www.virsi.lv/lv/privatpersonam/degviela/degvielas-un-elektrouzlades-cenas")
+#url_viada = requests.get("https://www.viada.lv/zemakas-degvielas-cenas/")
+#url_kool = requests.get("https://koolbusiness.lv/degvielas-cenu-vesture/")
+#url_gotika = requests.get("https://www.gotikaauto.lv")
 
-print(requests.get(url_virsi).text)
+
+url_circle = requests.get("https://www.circlek.lv/privātpersonām/degvielas-cenas")
+circle = BeautifulSoup(url_circle.content,"html.parser")
+circle = circle.find_all("table",{"class":"ck-striped-table uk-table uk-table-striped"})
+circlestr = ""
+
+for p in circle:
+    circlestr += p.text
+
+circlestr = circlestr.replace("Degviela","").replace("Cena EUR","").replace("Uzpildes stacijas adrese","").replace("Visos Rīgas DUS degvielas cenas ir vienādas.","")
+circlelist = circlestr.split()
+print(circlelist)
+
+circle_miles95 = circlelist[2].strip()
+circle_milesPLUS98 = circlelist[9].strip()
+circle_milesD = circlelist[16].strip()
+circle_milesPLUSD = circlelist[23].strip()
+circle_autogaze = circlelist[29].strip()
+
+
+
+
--- js-flask.py
+++ js-flask.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
 import time
+from fuelscrape import circle_miles95, circle_milesPLUS98, circle_milesD, circle_milesPLUSD, circle_autogaze
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,4 +15,5 @@ def weather():
 
 @app.route("/fuel")
 def fuel():
-	return render_template("fuel.html", datetime = str(time.ctime()))
+	
+	return render_template("fuel.html", datetime = str(time.ctime()),c95 = circle_miles95,c98 = circle_milesPLUS98,cmilesD = circle_milesD,cplusD = circle_milesPLUSD, cgaze = circle_autogaze)
